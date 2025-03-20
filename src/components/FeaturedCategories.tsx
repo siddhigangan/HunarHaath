a37c2aa0f4e3bb04bf90fd879104b0fb4a245b77
khushigilda <khushigildaamt.com>
2025-03-20 17:39:15 +0530

    ImageAdded

--- src/components/FeaturedCategories.tsx
+++ src/components/FeaturedCategories.tsx
@@ -21,7 +21,7 @@ export function FeaturedCategories() {
     },
     {
       name: "Home Decor",
-      image: "purse real.jfif",
+      image: "/purse real.jfif",
       path: "/category/home-decor"
     },
     {
